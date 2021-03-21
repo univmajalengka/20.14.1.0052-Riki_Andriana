8a0b4cbb4afeed46d056c38832271122b890b0cd
Riki Andriana <rikiandrianakuliah@gmail.com>
2021-03-21 23:03:45 +0700

    Tugas 1

---
+++ Tugas 1/Tugas1_Biodata.pas
@@ -0,0 +1,17 @@
+{Nama : Riki Andriana}
+{NPM : 20.14.1.0052}
+{Nama Program : Biodata}
+
+program biodata;
+uses crt;
+
+begin
+writeln('Nama : Riki Andriana');
+writeln('NPM : 20.14.1.0052');
+writeln('Alamat : Lemah Putih');
+writeln('TTL : Majalengka, 8 April 2002');
+writeln('No. HP : 082119105215');
+writeln('Motto : Hidup Seperti Larry');
+readln;
+
+end.
